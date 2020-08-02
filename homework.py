3b88e0c337aef3bc098aa118779a1bef63c0cf50
NightwolfCB <nightwolf63@rambler.ru>
2020-08-02 19:41:17 +0300

    Спринт 2 - калькулятор денег и калорий (исправления по PEP8 2)

--- homework.py
+++ homework.py
@@ -39,53 +39,38 @@ class Record:
 class CashCalculator(Calculator):
     USD_RATE = float(60.0)
     EURO_RATE = float(70.0)
-    
+
     def get_today_cash_remained(self, currency):
         cash_remained = self.limit - self.get_today_stats()
-        self.currency = currency
-        if currency == 'rub':
-            currency_name = "руб"
-            if cash_remained > 0:
-                return f'На сегодня осталось {cash_remained} {currency_name}'
-            elif cash_remained == 0:
-                return f'Денег нет, держись'
-            else:
-                cash_remained_abs = abs(cash_remained)
-                return f'Денег нет, держись: твой долг - {cash_remained_abs} {currency_name}'
+        if cash_remained == 0:
+            return f'Денег нет, держись'
+        currencys = {
+            'rub': ['руб', 1],
+            'usd': ['USD', CashCalculator.USD_RATE],
+            'eur': ['Euro', CashCalculator.EURO_RATE]
+            }
+        if currency not in currencys:
+            return f'Неизвестная валюта'
+        if currency in currencys:
+            now_cash = cash_remained / currencys[currency][1]
+        currency_name = currencys[currency][0]
+        if cash_remained > 0:
+            return f'На сегодня осталось {now_cash:.2f} {currency_name}'
+        now_cash_abs = abs(now_cash)
+        message = (
+            f'Денег нет, держись: твой долг - '
+            f'{now_cash_abs:.2f} {currency_name}'
+        )
+        return message
 
-        elif currency == 'usd':
-            cash_remained = round(cash_remained / (self.USD_RATE), 2)
-            currency_name = "USD"
-            if cash_remained > 0:
-                return f'На сегодня осталось {cash_remained} {currency_name}'
-            elif cash_remained == 0:
-                return f'Денег нет, держись'
-            else:
-                cash_remained_abs = abs(cash_remained)
-                return f'Денег нет, держись: твой долг - {cash_remained_abs} {currency_name}'
- 
-        elif currency == 'eur':
-            cash_remained = round(cash_remained / (self.EURO_RATE), 2)
-            currency_name = "Euro"
-            if cash_remained > 0:
-                return f'На сегодня осталось {cash_remained} {currency_name}'
-            elif cash_remained == 0:
-                return f'Денег нет, держись'
-            else:
-                cash_remained_abs = abs(cash_remained)
-                return f'Денег нет, держись: твой долг - {cash_remained_abs} {currency_name}'
 
-   
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_remained = self.limit - self.get_today_stats()
         if calories_remained > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories_remained} кКал'
+            message = (
+                f'Сегодня можно съесть что-нибудь ещё, '
+                f'но с общей калорийностью не более {calories_remained} кКал'
+            )
+            return message
         return f'Хватит есть!'
-
-
-cash_calculator = CashCalculator(1000)
-cash_calculator.add_record(Record(amount=145, comment="кофе"))
-cash_calculator.add_record(Record(amount=1000, comment="Серёге за обед"))
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="31.07.2020"))
-print(cash_calculator.get_today_cash_remained("rub"))
